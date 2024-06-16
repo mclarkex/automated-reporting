8c7d7a14b7b50e9f0c2eac737f354f13da324372
mclarkex <mclarke.1011010@gmail.com>
2024-06-16 20:59:09 +0100

    added workload dispatch

--- cloudmigrations/cloudDistribtuion.py
+++ cloudmigrations/cloudDistribtuion.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.patches import ConnectionPatch
 
 # Data
 labels = ['AWS Cloud Foundations', 'Azure Cloud Foundations', 'All other AWS', 'All other Azure']
@@ -14,97 +16,46 @@ colors = [aws_color, azure_color, other_aws_color, other_azure_color]
 explode = (0.1, 0.1, 0, 0)  # explode the first two slices
 
 # Create the pie chart
-fig, ax = plt.subplots(figsize=(10, 7))
+fig, ax = plt.subplots(figsize=(12, 9))
 wedges, texts, autotexts = ax.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%',
                                   shadow=True, startangle=140, textprops={'fontsize': 14}, pctdistance=0.85)
 
 # Draw a circle at the center to make it look like a donut chart
-centre_circle = plt.Circle((0, 0), 0.70, fc='white')
+centre_circle = plt.Circle((0, 0), 0.70, fc='white', linewidth=1.25, edgecolor='black')
 fig.gca().add_artist(centre_circle)
 
-# Customize the appearance of the pie chart
-for text in texts + autotexts:
+# Enhance the appearance of the pie chart
+for i, (wedge, text, autotext) in enumerate(zip(wedges, texts, autotexts)):
     text.set_color('#333333')  # set text color to dark grey
     text.set_fontsize(14)
+    autotext.set_color('white')  # set percentage text color to white
+    autotext.set_fontsize(14)
     
-# Equal aspect ratio ensures that pie is drawn as a circle.
-ax.axis('equal')
-
-plt.title('Current Cloud Distribution', fontsize=18, color='#333333', weight='bold')
-plt.tight_layout()
-plt.show()
-
-import matplotlib.pyplot as plt
-
-# Data
-labels = ['AWS Cloud Foundations', 'Azure Cloud Foundations', 'All other AWS', 'All other Azure']
-sizes = [5.72, 7.40, 28.88, 48.00]
-
-# Brand colors
-aws_color = '#FF9900'
-azure_color = '#0072C6'
-other_aws_color = '#FFD580'  # lighter shade of AWS orange
-other_azure_color = '#66B2FF'  # lighter shade of Azure blue
-colors = [aws_color, azure_color, other_aws_color, other_azure_color]
-
-explode = (0.05, 0.05, 0, 0)  # slight explode for the first two slices
-
-# Create the pie chart
-fig, ax = plt.subplots(figsize=(10, 7))
-wedges, texts, autotexts = ax.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%',
-                                  shadow=False, startangle=140, textprops={'fontsize': 14}, pctdistance=0.85)
-
-# Customize the appearance of the pie chart
-for text in texts:
-    text.set_color('#333333')  # set text color to dark grey
-    text.set_fontsize(14)
-
-for autotext in autotexts:
-    autotext.set_color('white')  # set percentage text color to white
-    autotext.set_fontsize(14)
+    # Add gradient effect to each wedge
+    wedge.set_edgecolor('white')
+    wedge.set_linewidth(2)
 
 # Equal aspect ratio ensures that pie is drawn as a circle.
 ax.axis('equal')
 
-plt.title('Current Cloud Distribution', fontsize=18, color='#333333', weight='bold')
+# Add annotations to enhance information
+for i, (text, wedge) in enumerate(zip(texts, wedges)):
+    # Create the connection lines
+    angle = (wedge.theta2 - wedge.theta1) / 2. + wedge.theta1
+    x = np.cos(np.deg2rad(angle))
+    y = np.sin(np.deg2rad(angle))
+
+    horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+    connection_style = "angle,angleA=0,angleB={}".format(angle)
+    connection = ConnectionPatch(
+        xyA=(0, 0), coordsA=ax.transData,
+        xyB=(x * 1.4, y * 1.4), coordsB=ax.transData,
+        arrowstyle="-", color='#333333', lw=1.5
+    )
+    ax.add_patch(connection)
+    text.set_horizontalalignment(horizontalalignment)
+    text.set_position((x * 1.5, y * 1.5))
+
+plt.title('Current Cloud Distribution', fontsize=22, color='#333333', weight='bold', pad=20)
 plt.tight_layout()
 plt.show()
-
-import matplotlib.pyplot as plt
-
-# Data
-labels = ['AWS Cloud Foundations', 'Azure Cloud Foundations', 'All other AWS', 'All other Azure']
-sizes = [5.72, 7.40, 28.88, 48.00]
-
-# Brand colors
-aws_color = '#FF9900'
-azure_color = '#0072C6'
-other_aws_color = '#FFD580'  # lighter shade of AWS orange
-other_azure_color = '#66B2FF'  # lighter shade of Azure blue
-colors = [aws_color, azure_color, other_aws_color, other_azure_color]
-
-# Create the pie chart
-fig, ax = plt.subplots(figsize=(10, 7))
-wedges, texts, autotexts = ax.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%',
-                                  shadow=False, startangle=140, textprops={'fontsize': 14}, pctdistance=0.85)
-
-# Customize the appearance of the pie chart
-for text in texts:
-    text.set_color('#333333')  # set text color to dark grey
-    text.set_fontsize(14)
-
-for autotext in autotexts:
-    autotext.set_color('white')  # set percentage text color to white
-    autotext.set_fontsize(14)
-
-# Equal aspect ratio ensures that pie is drawn as a circle.
-ax.axis('equal')
-
-# Add transparency to the background
-fig.patch.set_alpha(0.0)
-
-plt.title('Current Cloud Distribution', fontsize=18, color='#333333', weight='bold')
-plt.tight_layout()
-plt.show()
-
-
